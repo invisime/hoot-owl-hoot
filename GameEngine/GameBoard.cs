88f593b47a3e1dd5d2375aeee66135cc1d7711a6
Robert Herbig <sheltim@gmail.com>
2019-09-11 21:33:59 -0400

    Put an Owl on the game board

--- GameEngine/GameBoard.cs
+++ GameEngine/GameBoard.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GameEngine
 {
     public class GameBoard
-    {
+    { 
+        public int OwlPosition { get; set; }
+
+
+        public GameBoard()
+        {
+            OwlPosition = 0;
+        }
     }
 }
---
+++ GameEngineTests/GameBoardTests.cs
@@ -0,0 +1,20 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace GameEngineTests
+{
+    [TestClass]
+    public class GameBoardTests
+    {
+        [TestMethod]
+        public void ShouldStartOwlsAtFirstSpots()
+        {
+            var board = new GameBoard();
+            var actualPosition = board.OwlPosition;
+            var expectedPosition = 0;
+
+            Assert.AreEqual(expectedPosition, actualPosition);
+        }
+    }
+}
