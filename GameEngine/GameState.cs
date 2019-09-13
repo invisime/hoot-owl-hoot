0883b5f4e3d99d7c87cfb8d4c2e67ec55e3c8c1c
Robert Herbig <sheltim@gmail.com>
2019-09-12 22:17:08 -0400

    Create GameState which can take one turn

---
+++ GameEngine/GameState.cs
@@ -0,0 +1,27 @@
+﻿
+namespace GameEngine
+{
+    public class GameState
+    {
+        public GameBoard Board { get; private set; }
+        public Deck Deck { get; private set; }
+        public RandomPlayer Player { get; private set; }
+
+        public GameState()
+        {
+            Board = new GameBoard();
+            Deck = new Deck();
+            var hand = Deck.Draw(3);
+            Player = new RandomPlayer(hand);
+        }
+
+        public void TakeTurn()
+        {
+            var cardToPlay = Player.Play(Board);
+            Board.Move(cardToPlay);
+            Player.Discard(cardToPlay);
+            var newCards = Deck.Draw(1);
+            Player.AddCardsToHand(newCards);
+        }
+    }
+}
---
+++ GameEngineTests/GameStateTests.cs
@@ -0,0 +1,39 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTests
+{
+    [TestClass]
+    public class GameStateTests
+    {
+        [TestMethod]
+        public void ShouldStartPlayerWithThreeCards()
+        {
+            var state = new GameState();
+
+            Assert.AreEqual(3, state.Player.Hand.Count);
+        }
+
+        [TestMethod]
+        public void ShouldTakeOneTurn()
+        {
+            var state = new GameState();
+            CardType[] startingHand = new CardType[3];
+            state.Player.Hand.CopyTo(startingHand);
+            var startingOwlPosition = state.Board.OwlPosition;
+            var startingDeckSize = state.Deck.Cards.Count;
+
+            state.TakeTurn();
+
+            var endingHand = state.Player.Hand;
+            CollectionAssert.AreNotEqual(startingHand, endingHand);
+            Assert.AreEqual(3, endingHand.Count);
+
+            var endingOwlPosition = state.Board.OwlPosition;
+            Assert.AreNotEqual(startingOwlPosition, endingOwlPosition);
+
+            var endingDeckSize = state.Deck.Cards.Count;
+            Assert.AreEqual(startingDeckSize - 1, endingDeckSize);
+        }
+    }
+}
