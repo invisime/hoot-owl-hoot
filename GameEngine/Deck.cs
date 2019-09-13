99d8eb39b8dba13fba522a26e4555539b3eeaa27
Robert Herbig <sheltim@gmail.com>
2019-09-12 21:49:53 -0400

    Create Deck that can be shuffled and drawn from

---
+++ GameEngine/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameEngine
+{
+    public class Deck
+    {
+        private static readonly Random Random = new Random();
+        public List<CardType> Cards { get; private set; }
+
+        public Deck()
+        {
+            BuildDeck();
+            Shuffle();
+        }
+
+        public CardType Draw()
+        {
+            var card = Cards[0];
+            Cards.RemoveAt(0);
+            return card;
+        }
+
+        private void BuildDeck()
+        {
+            Cards = new List<CardType>();
+            for (int i = 0; i < 6; i++)
+            {
+                foreach (CardType type in Enum.GetValues(typeof(CardType)))
+                {
+                    Cards.Add(type);
+                }
+            }
+        }
+
+        private void Shuffle()
+        {
+            // http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+            int n = Cards.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = Random.Next(n + 1);
+                CardType value = Cards[k];
+                Cards[k] = Cards[n];
+                Cards[n] = value;
+            }
+        }
+    }
+}
---
+++ GameEngineTests/DeckTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTests
+{
+    [TestClass]
+    public class DeckTests
+    {
+        [TestMethod]
+        public void ShouldDrawZerothCard()
+        {
+            var deck = new Deck();
+            var expectedCard = deck.Cards[0];
+
+            var actualCard = deck.Draw();
+
+            Assert.AreEqual(expectedCard, actualCard);
+        }
+
+        [TestMethod]
+        public void ShouldRemoveDrawnCardFromTheDeck()
+        {
+            var deck = new Deck();
+            var deckSizeBefore = deck.Cards.Count;
+
+            deck.Draw();
+
+            var deckSizeAfter = deck.Cards.Count;
+            Assert.AreEqual(deckSizeBefore - 1, deckSizeAfter);
+        }
+    }
+}
