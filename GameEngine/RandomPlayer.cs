845d12923ddb0fe7bf0ca227af549e231899ed07
Robert Herbig <sheltim@gmail.com>
2019-09-12 20:33:15 -0400

    Create a Player which plays random cards from its hand

---
+++ GameEngine/RandomPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameEngine
+{
+    public class RandomPlayer
+    {
+        private static readonly Random Random = new Random();
+        public List<CardType> Hand { get; private set; }
+
+        public RandomPlayer(List<CardType> hand)
+        {
+            Hand = hand;
+        }
+
+        public CardType Play(GameBoard board)
+        {
+            int index = Random.Next(0, Hand.Count);
+            return Hand[index];
+        }
+    }
+}
---
+++ GameEngineTests/RandomPlayerTests.cs
@@ -0,0 +1,27 @@
+﻿using GameEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineTests
+{
+    [TestClass]
+    public class RandomPlayerTests
+    {
+        [TestMethod]
+        public void ShouldPlayCardFromHand()
+        {
+            var expectedCardType = CardType.Blue;
+            var hand = new List<CardType> { expectedCardType };
+            var player = new RandomPlayer(hand);
+            var board = new GameBoard();
+
+            var playedCard = player.Play(board);
+
+            Assert.AreEqual(expectedCardType, playedCard);
+        }
+    }
+}
