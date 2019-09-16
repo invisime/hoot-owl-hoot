a7fa9e28764d859fedb9a32391a70db40daebf21
Robert Herbig <sheltim@gmail.com>
2019-09-15 21:35:38 -0400

    Add .net standard 2.0 unit test project

---
+++ GETests_Standard/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GETests_Standard
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void ShouldPass()
+        {
+            Console.WriteLine("passed a test");
+        }
+    }
+}
