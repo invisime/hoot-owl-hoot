7034f9f72957bf7d5c3d0ae2d8f9c238665b42f3
jdwile <jdwile@sep.com>
2019-11-05 15:07:13 -0500

    Create EpsilonGreedyPlayer
    Creates a new player that follows the Epsilon
    Greedy algorithm, which randomly decides on
    exploration of new actions rather than always
    following the Greedy action.

---
+++ GameEngine/Players/EpsilonGreedyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GameEngine.Players
+{
+    public class EpsilonGreedyPlayer: GreedyPlayer
+    {
+        private static readonly Random Random = new Random();
+        private double Epsilon { get; set; }
+
+        public EpsilonGreedyPlayer(double epsilon) :
+            base()
+        {
+            Epsilon = epsilon;
+        }
+
+        public override Play FormulatePlay(GameBoard board)
+        {
+            double randomSampling = Random.NextDouble();
+
+            if (randomSampling < Epsilon)
+            {
+                return base.FormulatePlay(board);
+            }
+
+            return new Play(
+                Hand.RandomCard,
+                board.Owls.TrailingOwl
+            );
+        }
+    }
+}
---
+++ GameEngineTests/Players/EpsilonGreedyPlayerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using GameEngine;
+using GameEngine.Players;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTests.Players
+{
+    [TestClass]
+    public class EpsilonGreedyPlayerTests
+    {
+        private CardType[] OneCardOfEachColor
+        {
+            get
+            {
+                return new Deck(1).Cards
+                    .Where(card => card != CardType.Sun)
+                    .ToArray();
+            }
+        }
+
+        [TestMethod]
+        public void ShouldPlayCardOnOnlyOwlThatGoesFurthestWhenUsingGreedyStrategy()
+        {
+            var player = new EpsilonGreedyPlayer(1);
+            var board = new GameBoard(2, 1);
+            player.Hand.Add(OneCardOfEachColor);
+
+            var play = player.FormulatePlay(board);
+
+            Assert.AreEqual(CardType.Red, play.Card);
+            Assert.AreEqual(0, play.Position);
+        }
+
+        [TestMethod]
+        public void ShouldPlayCardOnOwlThatGoesFurthestWithHootingIntoNestWhenUsingGreedyStrategy()
+        {
+            var player = new EpsilonGreedyPlayer(1);
+            var board = new GameBoard(2);
+            board.Owls.Move(0, 6);
+            player.Hand.Add(OneCardOfEachColor);
+
+            var play = player.FormulatePlay(board);
+
+            Assert.AreEqual(CardType.Red, play.Card);
+            Assert.AreEqual(1, play.Position);
+        }
+
+        [TestMethod]
+        public void ShouldPlayCardAtRandomFromHandWhenUsingRandomStrategy()
+        {
+            var player = new EpsilonGreedyPlayer(0);
+            var board = new GameBoard(2, 1);
+            player.Hand.Add(OneCardOfEachColor);
+
+            var play = player.FormulatePlay(board);
+
+            CollectionAssert.Contains(OneCardOfEachColor, play.Card);
+            Assert.AreEqual(0, play.Position);
+        }
+    }
+}
