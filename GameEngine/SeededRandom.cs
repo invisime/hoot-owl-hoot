18454be2311397be023cdb7073527c96cd752b82
jdwile <jdwile@sep.com>
2019-11-06 10:55:09 -0500

    SeededRandomizer allows for global control of random seeds.
    w/ @invisime

---
+++ GameEngine/SeededRandom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public static class SeededRandom
+    {
+        private static Random Random;
+
+        public static int Seed
+        {
+            set { SetSeed(value); }
+        }
+
+        static SeededRandom()
+        {
+            int seed = new Random().Next(int.MinValue, int.MaxValue);
+            SetSeed(seed);
+        }
+
+        public static int Next()
+        {
+            return Random.Next();
+        }
+
+        public static int Next(int maxValue)
+        {
+            return Random.Next(maxValue);
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            return Random.Next(minValue, maxValue);
+        }
+
+        public static void NextBytes(byte[] buffer)
+        {
+            Random.NextBytes(buffer);
+        }
+
+        public static double NextDouble()
+        {
+            return Random.NextDouble();
+        }
+
+        private static void SetSeed(int seed)
+        {
+            Random = new Random(seed);
+            Console.Out.WriteLine("New random seed: " + seed);
+        }
+    }
+}
