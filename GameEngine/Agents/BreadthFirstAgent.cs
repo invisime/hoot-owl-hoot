0975b2265f2f4808faaa4fcd6260cdff9788e12e
Thom Root <tlroot@sep.com>
2019-11-14 15:38:30 -0500

    breadth first search

---
+++ GameEngine/Agents/BreadthFirstAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GameEngine.Agents
+{
+    public class BreadthFirstAgent : TreeSearchAgent
+    {
+        protected override SearchNode RemoveStrategicNode(IList<SearchNode> nodes)
+        {
+            // Since new nodes are added to the end of the node list,
+            // the first node will always be the lowest depth node.
+            var node = nodes[0];
+            nodes.Remove(node);
+            return node;
+        }
+    }
+}
