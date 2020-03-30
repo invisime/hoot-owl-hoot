16775ac20a3141240843b60bcf6f3f46d0cff2d8
David L. Inman <dlinman@sep.com>
2020-03-30 17:38:46 -0400

    we don't need the game engine changes

--- GameEngine/GameEngine.cs
+++ GameEngine/GameEngine.cs
@@ -7,9 +7,9 @@ namespace GameEngine
     {
         public void RunGame()
         {
-            var player = new RandomAgent();
+            var player = new LeastRecentCardAgent();
             int numberOfTurns = 0;
-            var game = new Game(4);
+            var game = new Game(10);
             while(!game.IsOver)
             {
                 game.TakeTurn(player);
