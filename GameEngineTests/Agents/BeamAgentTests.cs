ad17d091578698e9f5147b4f644f53c8b66583b9
David L. Inman <dlinman@sep.com>
2020-04-13 12:58:35 -0400

    beam agent test

---
+++ GameEngineTests/Agents/BeamAgentTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using GameEngine;
+using GameEngine.Agents;
+using GameEngine.Heuristics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameEngineTests.Agents
+{
+    [TestClass]
+    public class BeamAgentTests
+    {
+        [TestMethod]
+        public void ShouldThrowExceptionIfCannotDeterminePlay()
+        {
+            var agent = new BeamAgent(1, new WorstCaseNumberOfPlaysToGo());
+            var state = TestUtilities.GenerateTestState(0, 0, 0, 0);
+
+            Assert.ThrowsException<NoMoveFoundException>(() => agent.FormulatePlay(state));
+        }
+    }
+}
