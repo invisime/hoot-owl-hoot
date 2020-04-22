c9b4eb3988a57b415b97f59a4162120dab37b793
David L. Inman <dlinman@sep.com>
2020-04-22 13:42:08 -0400

    options for agents

--- AgentComparison/Options.cs
+++ AgentComparison/Options.cs
@@ -24,10 +24,75 @@ namespace AgentComparison
             => new BeamStackAgent(int.Parse(parameters["width"]), heuristic); 
     }
 
+    public class BeamAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=Beam --width=<integer> --heuristic=<heuristic-name>";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new BeamAgent(int.Parse(parameters["width"]), heuristic); 
+    }
+
+    public class RandomAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=Random";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new RandomAgent(); 
+    }
+
+    public class MaxMaxAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=MaxMax --depth=<integer> --heuristic=<heuristic-name>";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new MaxMaxAgent(int.Parse(parameters["depth"]), heuristic); 
+    }
+
+    public class LeastRecentCardAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=LeastRecentCard";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new LeastRecentCardAgent(); 
+    }
+
+    // TODO GreedyBestFirstAgent after refactor
+
+    public class GreedyAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=Greedy";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new GreedyAgent(); 
+    }
+
+    public class ExpectMaxAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=ExpectMax --depth=<integer> --heuristic=<heuristic-name>";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new ExpectMaxAgent(int.Parse(parameters["depth"]), heuristic); 
+    }
+
+    public class EpsilonGreedyAgentOption : IAgentOption 
+    { 
+        public string Description() => "Usage: AgentComparison --agent=EpsilonGreedy --epsilon=<double>";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new EpsilonGreedyAgent(double.Parse(parameters["epsilon"])); 
+    }
+
+    public class BreadthFirstAgentOption : IAgentOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=BreadthFirst";
+        public IAgent CreateAgent(IHeuristic heuristic, IReadOnlyDictionary<string, string> parameters) 
+            => new BreadthFirstAgent(); 
+    }
+
+    // TODO AStarAgent after refactor
+
     public class BestCaseNumberOfPlaysToGoHeuristicOption : IHeuristicOption
     {
         public string Description() => "Usage: AgentComparison --agent=<agent-name> --heuristic=BestNumberOfPlays";
         public IHeuristic CreateHeuristic(IReadOnlyDictionary<string, string> parameters) => new BestCaseNumberOfPlaysToGo();
+    }
+    public class WorstCaseNumberOfPlaysToGoHeuristicOption : IHeuristicOption
+    {
+        public string Description() => "Usage: AgentComparison --agent=<agent-name> --heuristic=WorstNumberOfPlays";
+        public IHeuristic CreateHeuristic(IReadOnlyDictionary<string, string> parameters) => new WorstCaseNumberOfPlaysToGo();
     }
 
     public class Options
@@ -40,12 +105,21 @@ namespace AgentComparison
         {
             Agents = new Dictionary<string, IAgentOption>
             {
-                { "BeamStack", new BeamStackAgentOption() }
+                { "BeamStack", new BeamStackAgentOption() },
+                { "Beam", new BeamAgentOption() },
+                { "Random", new RandomAgentOption() },
+                { "MaxMax", new MaxMaxAgentOption() },
+                { "LeastRecentCard", new LeastRecentCardAgentOption() },
+                { "Greedy", new GreedyAgentOption() },
+                { "ExpectMax", new ExpectMaxAgentOption() },
+                { "EpsilonGreedy", new EpsilonGreedyAgentOption() },
+                { "BreadthFirst", new BreadthFirstAgentOption() },
             };
 
             Heuristics = new Dictionary<string, IHeuristicOption>
             {
-                { "BestNumberOfPlays", new BestCaseNumberOfPlaysToGoHeuristicOption() }
+                { "BestNumberOfPlays", new BestCaseNumberOfPlaysToGoHeuristicOption() },
+                { "WorstNumberOfPlays", new WorstCaseNumberOfPlaysToGoHeuristicOption() }
             };
         }
 
