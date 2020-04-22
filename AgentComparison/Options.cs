0778759119a6b44e0ebd04853cc5064635bf27d8
David L. Inman <dlinman@sep.com>
2020-04-21 21:37:34 -0400

    outline for how to select agents

---
+++ AgentComparison/Options.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+using GameEngine.Agents;
+using GameEngine.Heuristics;
+
+namespace AgentComparison
+{
+    public interface IAgentOption 
+    {
+        string Description(); 
+        IAgent CreateAgent(IHeuristic heuristic);
+    }
+
+    public interface IHeuristicOption
+    {
+        string Description();
+        IAgent CreateHeuristic();
+    }
+
+    public class BeamStackAgentOption : IAgentOption
+    {
+        public string Description() => "Beam Stack Agent";
+        public IAgent CreateAgent(IHeuristic heuristic) => new BeamStackAgent(0, heuristic); // TODO extra params?
+    }
+
+    public class Options
+    {
+        private readonly IReadOnlyDictionary<string, IAgentOption> _agents;
+        private readonly IReadOnlyDictionary<string, IHeuristicOption> _heuristics;
+        public IReadOnlyDictionary<string, IAgentOption> Agents => _agents;
+
+        public IReadOnlyDictionary<string, IHeuristicOption> Heuristics => _heuristics;
+
+        public Options() 
+        {
+            _agents = new Dictionary<string, IAgentOption>
+            {
+                { "BeamStack", new BeamStackAgentOption() }
+            };
+        }
+
+    }
+}
--- AgentComparison/Program.cs
+++ AgentComparison/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using GameEngine;
+using GameEngine.Agents;
+using GameEngine.Heuristics;
 
 namespace AgentComparison
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var player = new LeastRecentCardAgent();
+            int numberOfTurns = 0;
+            var game = new Game(10);
+            while(!game.IsOver)
+            {
+                game.TakeTurn(player);
+                numberOfTurns++;
+            }
+            Console.WriteLine("Game is {0}!", game.IsLost ? "lost" : "won");
+            Console.WriteLine("Game took {0} turns!", numberOfTurns);
         }
     }
 }
